4aaae2d9c4008461d50ba98836aed37a1c7e1765
Vedananda-19 <pvedananda@gmail.com>
2026-04-24 10:25:03 +0530

    Added Login Page

--- frontend/pages/LoginPage.jsx
+++ frontend/pages/LoginPage.jsx
@@ -1,8 +1,35 @@
+import Button from "../components/Button";
+import styles from "./LoginPage.module.css";
+import {Link} from "react-router-dom";
 
-function LoginPage(){
+function LoginPage() {
+
+    const handleLogin = () => {
+        return;
+    }
+
     return (
-        <h1>Hello World</h1>
-    )
+        <div className="themeBackground">
+            <div className="fogOverlay"></div>
+            <div className="centerCard">
+                <h1>Scotland Yard</h1>
+                <div className={styles.inputGroup}>
+                    <input
+                        className={styles.loginInput}
+                        type="text"
+                        placeholder="Username"
+                    ></input>
+                    <input
+                        className={styles.loginInput}
+                        type="password"
+                        placeholder="Password"
+                    ></input>
+                    <Button onClick={handleLogin} name="Sign In" primary/>
+                    <p className={styles.signUpText}>Haven't Registered yet? Click Here to <Link className={styles.signUpLink}>Sign Up</Link></p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
